4f0fc56e391bfef1e58f6d9eeccda92e1fe682f6
TS22082 <ts22082@gmail.com>
2019-11-22 10:38:53 -0800

    create trainer object

---
+++ assets/trainer.js
@@ -0,0 +1,38 @@
+const Pokemon = require("./pokemon");
+
+function Trainer(name) {
+  this.name = name;
+  this.currentPokemon;
+  this.pokemon = [
+    new Pokemon("Squirtle"),
+    new Pokemon("Charmander"),
+    new Pokemon("Bulbasaur")
+  ];
+}
+
+Trainer.prototype.showPokemon = function() {
+  console.log(this.pokemon);
+};
+
+Trainer.prototype.setCurrentPokemon = function(poke) {
+  this.pokemon.forEach((element, index) => {
+    if (element.name === poke) {
+      this.currentPokemon = element;
+      this.pokemon.splice(index, 1);
+    }
+  });
+};
+
+Trainer.prototype.switchPokemon = function(poke) {
+  this.pokemon.forEach((element, index) => {
+    if (element.name === poke) {
+      const pokeToSwitch = this.pokemon[index];
+      this.pokemon[index] = this.currentPokemon;
+      this.currentPokemon = pokeToSwitch;
+    }
+  });
+};
+
+const Thomas = new Trainer("Thomas");
+
+module.exports = Trainer;
